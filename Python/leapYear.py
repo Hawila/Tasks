d2f3ac0d52d9ccd53bf7274658261509b1ffc8ee
Hawila <abdulrahman.hawila@gmail.com>
2023-01-09 18:32:28 +0200

    Create leapYear.py

---
+++ Python/leapYear.py
@@ -0,0 +1,13 @@
+# one line func if condition consist of  2 conditions first one for century year and the second for normal year
+# it will return true if any of 2 conditions is true else will return False
+def is_leap(year):
+    return (year % 400 == 0 and year % 100 == 0) or (year % 4 == 0 and year % 100 != 0)
+
+
+print(is_leap(1800))  # False
+
+print(is_leap(2000))  # True
+
+print(is_leap(2400))  # True
+
+print(is_leap(2500))  # False
